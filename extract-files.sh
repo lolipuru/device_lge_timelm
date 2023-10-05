1f6cf027f319e87725257c249d526946ef08027a
me-cafebabe <me.cafebabe@gmail.com>
2023-10-06 04:32:27 +0800

    timelm: libstdc++.vendor => libstdc++_vendor
    vndksupport: Could not load /vendor/lib/hw/camera.sdm660.so from default namespace: dlopen failed: library "libstdc++.so" not found: needed by /vendor/lib/libdualcameraddm.so
    
    Change-Id: I478eb89aa2ac106d855b168844863032612bc8e8

--- extract-files.sh
+++ extract-files.sh
@@ -6,6 +6,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+function blob_fixup() {
+    case "${1}" in
+        vendor/lib/libSNPE.so|vendor/lib*/liblgdnnsnpe.so|vendor/lib*/liblgsnpeawb.so|vendor/lib*/liblgsnpeawb_beta.so|vendor/lib*/libtof_cal.so)
+            ${PATCHELF} --replace-needed libstdc++.so libstdc++_vendor.so "${2}"
+            ;;
+    esac
+}
+
 # If we're being sourced by the common script that we called,
 # stop right here. No need to go down the rabbit hole.
 if [ "${BASH_SOURCE[0]}" != "${0}" ]; then
